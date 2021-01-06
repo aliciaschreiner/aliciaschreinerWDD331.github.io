2f5c7787fb639027c5dea231549ac8c3a3177071
aliciaschreiner <59591459+aliciaschreiner@users.noreply.github.com>
2021-01-06 12:42:40 -0800

    Delete wdd331portal.js

--- aliciaschreinerWDD331.github.io/js/wdd331portal.js
+++
@@ -1,7 +0,0 @@
-let date = new Date();
-let year = date.getFullYear();
-
-let lastModify = document.lastModified;
-
-document.getElementById("currentyear").textContent = year;
-document.getElementById("lastmod").textContent = "Last Updated: " + lastModify;
